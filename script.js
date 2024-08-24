ca7050a4474d1161e062fface72e884e03d146f2
MarikaShub <city-sma@mail.ru>
2024-08-25 00:38:31 +0300

    fix: change function

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-const firstPrice = document.querySelector('.first-price')
-const secondPrice = document.querySelector('.second-price')
-const thirdPrice = document.querySelector('.third-price')
-const fourthPrice = document.querySelector('.fourth-price')
-result = +firstPrice.innerHTML + +secondPrice.innerHTML + +thirdPrice.innerHTML + +fourthPrice.innerHTML
+const firstPrice = document.querySelector(".first-price");
+const secondPrice = document.querySelector(".second-price");
+const thirdPrice = document.querySelector(".third-price");
+const fourthPrice = document.querySelector(".fourth-price");
+result =
+  +firstPrice.innerHTML +
+  +secondPrice.innerHTML +
+  +thirdPrice.innerHTML +
+  +fourthPrice.innerHTML;
 
-console.log(result)
-const totalCost = document.querySelector('.total-cost__paragraph')
-totalCost.textContent = result
+console.log(result);
+const totalCost = document.querySelector(".total-cost__paragraph");
+totalCost.textContent = result;
 
-const discountButton = document.querySelector('.discount')
-discountButton.addEventListener('click', function () {
+const discountButton = document.querySelector(".discount");
+
+discountButton.addEventListener(
+  "click",
+  function () {
     result *= 0.8;
     totalCost.textContent = result;
-})
+  },
+  { once: true }
+);
